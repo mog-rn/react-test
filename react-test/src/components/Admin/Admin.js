3aeb43e9bdd9ed718bfb6af56f29ea0569f060d0
Mogakamo <mogaka.amo254@gmail.com>
2021-03-15 15:21:02 +0300

    Commit

--- react-test/src/components/Admin/Admin.js
+++ react-test/src/components/Admin/Admin.js
@@ -2,11 +2,7 @@ import React, { Component } from 'react'
 
 
 const data  = [
-        {entry: 'AdminEntry1'},
-        {entry: 'AdminEntry2'},
-        {entry: 'AdminEntry3'},
-        {entry: 'AdminEntry4'},
-        {entry: 'AdminEntry5'}
+        
     ];
 const columns = [{
     dataField: 'entry',
@@ -14,12 +10,34 @@ const columns = [{
 }]
 
 class Admin extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            entries: [
+                {
+                    entry: 'AdminEntry1'
+                }, 
+                {
+                    entry: 'AdminEntry2'
+                }, 
+                {
+                    entry: 'AdminEntry3'
+                }, 
+                {
+                    entry: 'AdminEntry4'
+                }, 
+                {
+                    entry: 'AdminEntry5'
+                }
+            ]
+        }
+    }
     render() {
         return (
             <div>
                 <p>Admin Entries</p>
-                
-            </div>
+
+                </div>
 
         )
     }
