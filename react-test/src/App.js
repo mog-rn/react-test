4fcac4acf87147593bbc68b2dbdc1b32bbf3a11c
Mogakamo <mogaka.amo254@gmail.com>
2021-03-15 12:05:03 +0300

    Commit

--- react-test/src/App.js
+++ react-test/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import ToggleSwitch from '../src/ToggleSwitch/ToggleSwitch';
 
 function App() {
   return (
@@ -9,8 +10,15 @@ function App() {
             Logo
           </div>
           <div className="box-2">
-            
+            <ToggleSwitch />
           </div>
+          <div className="box-3">
+            <ul className="list-1">
+              <li></li>
+              <li></li> 
+              <li></li> 
+            </ul>  
+          </div>   
         </div>
       </div>
     </div>
