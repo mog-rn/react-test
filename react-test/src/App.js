47fde579a4a1c4078892ab9ba8f12b117698dc75
Mogakamo <mogaka.amo254@gmail.com>
2021-03-15 11:25:42 +0300

    Commit

--- react-test/src/App.js
+++ react-test/src/App.js
@@ -1,10 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <div className="container">
+        <div className="row">
+          <div className="box-1">
+            Logo
+          </div>
+          <div className="box-2">
+            
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
