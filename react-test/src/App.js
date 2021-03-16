5e5467e8e1b7772e904d5d1f0f39552f393dedc5
Mogakamo <mogaka.amo254@gmail.com>
2021-03-16 15:23:48 +0300

    Commit

--- react-test/src/App.js
+++ react-test/src/App.js
@@ -5,6 +5,21 @@ class App extends React.Component {
   constructor(props) {
     super(props);
 
+    this.state = {
+      app: 'none',
+      admin: 'none'
+    }
+
+    this.handleClick = this.handleClick.bind(this)
+  }
+  handleClick (e) {
+    if(e.target.value === 'app') {
+      this.setState({ app: '' })
+      this.setState({ admin: 'none' })
+    } else if (e.target.value === 'admin') {
+      this.setState({ admin: '' })
+      this.setState({ app: 'none' })
+    }
   }
   render() {
     return (
@@ -14,17 +29,23 @@ class App extends React.Component {
           <div className="logo">
             Logo
           </div>
-          <div className="radio-buttons">
-            <input type="radio" value="admin" /> Admin <br /><br />
-            <input type="radio" value="app" /> App
+          <div className="radio-buttons" onChange={this.handleClick}>
+            <input type="radio" value="admin"  /> Admin <br /><br />
+            <input type="radio" value="app"  /> App
+            
           </div>
-          <div className="admin">
-
-          </div>
-          <div className="app">
+          <div className="admin" style={{display: this.state.admin}}>
             <ul>
-              <li></li>
-              <li></li>
+              <li>AdminEntry1</li>
+              <li>AdminEntry2</li>
+              <li>AdminEntry3</li>
+            </ul>
+          </div>
+          <div className="app-1" style={{display: this.state.app}}>
+            <ul>
+              <li>Entry1</li>
+              <li>Entry2</li>
+              <li>Entry3</li>
             </ul>
           </div>
         </div>
