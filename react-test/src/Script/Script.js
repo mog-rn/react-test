58a72da5276129df4b9922f1d03901e747b78619
Mogakamo <mogaka.amo254@gmail.com>
2021-03-16 09:11:57 +0300

    Commit

---
+++ react-test/src/Script/Script.js
@@ -0,0 +1,9 @@
+function showAdmin () {
+    let element = document.getElementById('box-3');
+    element.style.display="block";
+} 
+
+function showApp () {
+    let element = document.getElementById('box-4');
+    element.style.display="block";
+}
